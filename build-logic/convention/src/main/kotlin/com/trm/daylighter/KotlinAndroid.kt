a38fb32dc297bd97a3344d36deca76af16c6ebcf
tosoba <therealmerengue@gmail.com>
2026-05-13 21:51:07 +0200

    Remove kotlin.Experimental opt-in from compiler arguments

--- build-logic/convention/src/main/kotlin/com/trm/daylighter/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/trm/daylighter/KotlinAndroid.kt
@@ -57,7 +57,6 @@ internal fun Project.configureKotlinAndroid(commonExtension: CommonExtension) {
         // Enable experimental coroutines APIs, including Flow
         "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
         "-opt-in=kotlinx.coroutines.FlowPreview",
-        "-opt-in=kotlin.Experimental",
       )
 
       jvmTarget.set(JvmTarget.JVM_21)
